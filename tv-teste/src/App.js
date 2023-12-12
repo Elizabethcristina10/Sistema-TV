5f8a3760ef803861d44d7eafdaa042da6f3b9469
Elizabethcristina10 <elizabethcristina2908@gmail.com>
2023-12-12 20:12:27 -0300

    teste commit

--- tv-teste/src/App.js
+++ tv-teste/src/App.js
@@ -1,12 +1,26 @@
 import './App.css';
 
 function App() {
+
+  <div class="container">
+      <div class="cidade"></div>
+      <div class="temperatura"></div>
+      <div class="informacoes"></div>
+      <div class="velocidade"></div>
+      <div class="distancia"></div>
+      <div class="nascer-sol"></div>
+      <div class="por-sol"></div>
+      <div class="horario"></div>
+  </div>
+
+
   return (
     <div className="App">
       <h1>Meu app de react</h1>
-      <p>Teste!!!!</p>
+      <p>Teste feito</p>
     </div>
   );
 }
 
 export default App;
+
